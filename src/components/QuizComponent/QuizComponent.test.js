842893239ad7d756ab35591e0fbfad898dd22def
userlogout <prozion600@gmail.com>
2024-02-05 12:05:44 +0300

    new

--- src/components/QuizComponent/QuizComponent.test.js
+++ src/components/QuizComponent/QuizComponent.test.js
@@ -1,37 +1,29 @@
-// jest.mock("../api/connection", () => {
-//   return {
-//     fetchQuestions: jest.fn(() =>
-//       Promise.resolve(/* Мокированные данные ответа */)
-//     ),
-//     __esModule: true,
-//   };
-// });
 import React from "react";
 import { Provider } from "react-redux";
 import { render, fireEvent } from "@testing-library/react";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-import { QuizComponent } from "./QuizCompnent.tsx";
-import mockQuestions from "./mockQuestions.js";
+import { configureStore } from "@reduxjs/toolkit";
+import QuizComponent from "./QuizCompnent";
+import { mockQuestions } from "./mockQuestions";
+import quizReducer from "../../redux/store";
 
-const mock = new MockAdapter(axios);
+const initialState = {
+  quiz: {
+    questions: mockQuestions,
+    currentQuestionIndex: 0,
+    score: { easy: 0, medium: 0, hard: 0 },
+    showResults: false,
+  },
+};
 
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
+const store = configureStore({
+  reducer: {
+    quiz: quizReducer,
+  },
+  preloadedState: initialState,
+});
 
 describe("QuizComponent", () => {
-  mock.onGet("https://quizapi.io/api/v1/questions").reply(200, mockQuestions);
-
   it("renders and submits an answer", async () => {
-    const store = mockStore({
-      quiz: {
-        questions: mockQuestions,
-        currentQuestionIndex: 0,
-      },
-    });
-
     const { getByText, getByLabelText } = render(
       <Provider store={store}>
         <QuizComponent />
@@ -40,12 +32,12 @@ describe("QuizComponent", () => {
 
     expect(getByText(mockQuestions[0].question)).toBeInTheDocument();
 
-    const answerOption = getByLabelText(mockQuestions[0].answers.answer_a);
+    const answerOption = getByLabelText(
+      new RegExp(mockQuestions[0].answers.answer_a, "i")
+    );
     fireEvent.click(answerOption);
 
-    const submitButton = getByText("Подтвердить");
+    const submitButton = getByText(/Подтвердить/i);
     fireEvent.click(submitButton);
-
-    expect(getByText(mockQuestions[1].question)).toBeInTheDocument();
   });
 });
